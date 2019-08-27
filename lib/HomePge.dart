424be4f99ccd7c6dc001af15d08ad05d1109b270
syssun <2506715686@qq.com>
2019-08-27 22:46:04 +0800

    1

--- lib/HomePge.dart
+++ lib/HomePge.dart
@@ -14,15 +14,19 @@ class Page extends State<HomePage>{
   Widget layout(BuildContext context){
     return new Scaffold(
       appBar: buildAppBar(context),
-      body: GridView(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 3,
-        mainAxisSpacing: 10.0,
-        crossAxisSpacing: 2.0,
-        childAspectRatio: 1.4
+      body: new Container(
+        margin: new EdgeInsets.only(top: 20.0),
+        child: GridView(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            mainAxisSpacing: 15.0,
+            crossAxisSpacing: 2.0,
+            childAspectRatio: 1.4
+        ),
+          children: gridViewList(context),
+        ),
+      )
 
-      ),
-      children: gridViewList(context),
-      ),
+
     );
   }
   Widget buildAppBar(BuildContext context){
@@ -32,18 +36,19 @@ class Page extends State<HomePage>{
     return new Image.network('https://img.mukewang.com/5c18cf540001ac8206000338-240-135.jpg');
   }
   Image getImage(src){
+    print(src);
     return new Image.asset(src,width: 50.0,height: 50.0,);
   }
   Padding getText(title){
+    TextStyle textStyle = new TextStyle(fontWeight: FontWeight.bold, fontSize: 14.0);
     return new Padding(
-        padding: const EdgeInsets.only(top: 5),
-        child: new Text(title,style: new TextStyle(fontSize: 18.0,))
+        padding: const EdgeInsets.only(top: 10),
+        child: new Text(title,style: textStyle)
     );
 
   }
 List<Widget> gridViewList(BuildContext context){
     List<Widget> widlist = new List();
-    List<Map<String,String>> titles = new List();
     widlist.add(buildItem(context,{"title":"锁屏", "src":"image/suoping.png"}));
     widlist.add(buildItem(context,{"title":"解锁", "src":"image/cancet.png"}));
     widlist.add(buildItem(context,{"title":"60S关机", "src":"image/shutdow.png"}));
@@ -56,7 +61,7 @@ List<Widget> gridViewList(BuildContext context){
 }
   Widget buildItem(BuildContext context,Map map){
     //设置字体样式
-    TextStyle textStyle = new TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0);
+
     return new GestureDetector(
           onTap: (){
 
